cd9819f522005ba1dbc7edf33912284109a37c4c
Mateus V. Gomes <monkmitogames@hotmail.com>
2023-05-19 01:59:32 +0000

    Todos os verbos inseridos

--- script.js
+++ script.js
@@ -13,8 +13,64 @@ fetch("https://reqres.in/api/users/1", {
   })
   .then((data) => {
     storedData = data;
-    const message = storedData?`O nome do usuário digitado é ${storedData.data.first_name} ${storedData.data.last_name} `:'';
-        console.log(message);
+    const message = storedData
+      ? `O nome do usuário digitado é ${storedData.data.first_name} ${storedData.data.last_name} `
+      : "";
+    console.log(message);
+  })
+  .catch((error) => {
+    console.log("Erro:", error);
+  });
+const newUser = {
+  id: 15,
+  email: "jorge@gmail.com",
+  first_name: "Jorge",
+  last_name: "Gomes",
+  avatar: "https://reqres.in/img/faces/1-image.jpg",
+};
+
+fetch("https://reqres.in/api/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(newUser),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log("Erro:", error);
+  });
+
+fetch("https://reqres.in/api/users/2", {
+  method: "DELETE",
+})
+  .then((response) => {
+    if (response.ok) {
+      console.log("Usuário excluído com sucesso");
+    } else {
+      console.log("Erro ao excluir o usuário");
+    }
+  })
+  .catch((error) => {
+    console.log("Erro:", error);
+  });
+const updatedUser = {
+  name: "John Doe",
+  job: "Developer",
+};
+fetch("https://reqres.in/api/users/2", {
+  method: "PUT",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(updatedUser),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
   })
   .catch((error) => {
     console.log("Erro:", error);
